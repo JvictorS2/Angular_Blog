1a88b594b8f6850228ed4aa95c1d768adecd204d
JvitorS2 <vssimoesdunck@gmail.com>
2023-04-28 14:50:19 -0300

    class finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": '1',
+        'photoCover': 'https://th.bing.com/th/id/OIP.lC__MSWQaAgpCBGQTwZg_wHaEo?pid=ImgDet&rs=1',
+        'title': 'Bioshock 4 é anunciado',
+        "description": 'De acordo com a equipe de desenvolvimento da 2k o próximo titulo da franquia bioshock está em desenvolvimento, este titulo será desenvolvido por uma nova equipe chamada \'Cloud Chamber\' e contará com o escritor de far cry 5 e ghost of tsushima'
+    },
+    {
+        "id": '2',
+        'photoCover': 'https://th.bing.com/th/id/OIP.sk9QfHCD2-68cEOwFDvk6QHaEK?pid=ImgDet&rs=1',
+        'title': 'Bioshock uma masterpeace',
+        "description": 'De acordo com a equipe de desenvolvimento da 2k o próximo titulo da franquia bioshock está em desenvolvimento, este titulo será desenvolvido por uma nova equipe chamada \'Cloud Chamber\' e contará com o escritor de far cry 5 e ghost of tsushima'
+    },
+    {
+        "id": '3',
+        'photoCover': 'https://th.bing.com/th/id/R.4f40d7cc28ca0d9b5d3f41187279b737?rik=KfQ4l7aHynJ20w&pid=ImgRaw&r=0',
+        'title': 'Bioshock 2, uma história de pai e filha',
+        "description": 'De acordo com a equipe de desenvolvimento da 2k o próximo titulo da franquia bioshock está em desenvolvimento, este titulo será desenvolvido por uma nova equipe chamada \'Cloud Chamber\' e contará com o escritor de far cry 5 e ghost of tsushima'
+    },
+    {
+        "id": '4',
+        'photoCover': 'https://th.bing.com/th/id/R.37b0b53287e4b5723eccfe06ad821362?rik=9%2fjANt4n1LpOUQ&pid=ImgRaw&r=0',
+        'title': 'Bioshock infinite e sua infinitas posibilidades',
+        "description": 'De acordo com a equipe de desenvolvimento da 2k o próximo titulo da franquia bioshock está em desenvolvimento, este titulo será desenvolvido por uma nova equipe chamada \'Cloud Chamber\' e contará com o escritor de far cry 5 e ghost of tsushima'
+    }
+]
